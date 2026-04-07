422fd90a3f619ee41c9c0da7e7eb695514a7be04
douaa <douaalahmar24@gmail.com>
2026-04-08 05:58:39 +0800

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center">Hello World</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
