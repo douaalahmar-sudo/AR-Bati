5d25a86c7f29f09c0566a8fe8ceb98086ed9fc7b
douaa <douaalahmar24@gmail.com>
2026-04-11 04:38:24 +0800

    add redux persist

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// vite.config.js
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://127.0.0.1:3000', // Change 'localhost' to '127.0.0.1'
         secure: false,
       },
     },
