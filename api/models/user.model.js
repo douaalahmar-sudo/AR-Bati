4d63172e1a11063aa57bf8f69e72846814700efb
douaa <douaalahmar24@gmail.com>
2026-04-18 18:05:16 +0800

    add delete user functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,6 +19,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         default:"https://imgs.search.brave.com/lL90r4r89CxNdhFFAGQqRT3PYO6GF-xdyIAu2M4g4-Q/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/aXByY2VudGVyLmdv/di9pbWFnZS1yZXBv/c2l0b3J5L2JsYW5r/LXByb2ZpbGUtcGlj/dHVyZS5wbmcvQEBp/bWFnZXMvaW1hZ2Uu/cG5n"
     },
+    /*role: {
+  type: String,
+  enum: ["admin", "client", "contractor"],
+  default: "client"
+},*/
 }, { timestamps: true }); // yaml createdAt w updatedAt automatiquement
 
 const User = mongoose.model("User", userSchema);
